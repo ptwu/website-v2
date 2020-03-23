cc0cc9dd8b5d93719c7c91283a68577eab033238
Peter Wu <peterlingwu@gmail.com>
2020-03-23 18:40:53 -0400

    Add eslint and HeaderCard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,45 @@
 import React from 'react';
 import './App.css';
-import Card from '@material-ui/core/Card';
-import Container from '@material-ui/core/Container';
+import { ThemeProvider, CssBaseline } from '@material-ui/core';
+import { createMuiTheme } from '@material-ui/core/styles';
+import HeaderCard from './components/HeaderCard/HeaderCard';
+import MetropolisOtf from './font/Metropolis-Medium.otf';
+
+const metropolis = {
+  fontFamily: 'Metropolis',
+  fontStyle: 'normal',
+  fontDisplay: 'swap',
+  fontWeight: 400,
+  src: `
+    local('Metropolis'),
+    local('Metropolis-Medium'),
+    local('Metropolis-Regular'),
+    url(${MetropolisOtf}) format('opentype')
+  `,
+  unicodeRange:
+    'U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF',
+};
 
 const theme = createMuiTheme({
   typography: {
-    fontFamily: [
-      '-apple-system',
-      'BlinkMacSystemFont',
-      '"Segoe UI"',
-      'Roboto',
-      '"Helvetica Neue"',
-      'Arial',
-      'sans-serif',
-      '"Apple Color Emoji"',
-      '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"',
-    ].join(','),
+    fontFamily: 'Metropolis',
+  },
+  overrides: {
+    MuiCssBaseline: {
+      '@global': {
+        '@font-face': [metropolis],
+      },
+    },
   },
 });
 
+
 function App() {
   return (
-    <>
-      <Container>
-        <Card> Hello </Card>
-      </Container>
-    </>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <HeaderCard />
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/HeaderCard/HeaderCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import Container from '@material-ui/core/Container';
+import './HeaderCard.css';
+
+export default () => (
+  <Container maxWidth="md">
+    <Card>
+      <h1>Peter Wu</h1>
+    </Card>
+  </Container>
+);
