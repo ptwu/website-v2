400b22bb1c66eb49ef0047b9ed7ba41a6282ce82
Peter Wu <peterlingwu@gmail.com>
2020-03-24 18:07:24 -0400

    Complete HeaderCard component

--- src/components/HeaderCard/HeaderCard.jsx
+++ src/components/HeaderCard/HeaderCard.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
-import { Card, Container, IconButton } from '@material-ui/core';
+import {
+  Card, Container, IconButton, Grid,
+} from '@material-ui/core';
 import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import AssignmentIndIcon from '@material-ui/icons/AssignmentInd';
 import styles from './HeaderCard.module.css';
+import ResumePDF from '../../docs/Peter_Wu_Resume.pdf';
 
 export default () => (
   <Container maxWidth="md">
@@ -10,11 +15,29 @@ export default () => (
         <h1>Peter Wu</h1>
         <p>
           Hey there! I&apos;m Peter, and I&apos;m a CS major at Cornell
-          University who likes to make things that work (but this website does not).
+          University who likes to make things that work.
         </p>
-        <IconButton className={styles.portfolioButton} aria-label="delete">
-          <GitHubIcon />
-        </IconButton>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Grid container justify="center" spacing="8">
+              <Grid item>
+                <IconButton className={styles.portfolioButton} target="_blank" rel="noopener noreferrer" href="https://github.com/ptwu" aria-label="GitHub">
+                  <GitHubIcon />
+                </IconButton>
+              </Grid>
+              <Grid item>
+                <IconButton className={styles.portfolioButton} target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/peterlingwu/" aria-label="LinkedIn">
+                  <LinkedInIcon />
+                </IconButton>
+              </Grid>
+              <Grid item>
+                <IconButton className={styles.portfolioButton} target="_blank" rel="noopener noreferrer" href={ResumePDF} aria-label="Resume">
+                  <AssignmentIndIcon />
+                </IconButton>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
       </Container>
     </Card>
   </Container>
