89003acd728c99b7ab436c6114abb76b09c648b9
Peter Wu <peterlingwu@gmail.com>
2020-03-24 04:37:36 -0400

    HeaderCard icon buttons

--- src/components/HeaderCard/HeaderCard.jsx
+++ src/components/HeaderCard/HeaderCard.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
-import Card from '@material-ui/core/Card';
-import Container from '@material-ui/core/Container';
-import './HeaderCard.css';
+import { Card, Container, IconButton } from '@material-ui/core';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import styles from './HeaderCard.module.css';
 
 export default () => (
   <Container maxWidth="md">
-    <Card>
-      <h1>Peter Wu</h1>
+    <Card className={styles.headerCard}>
+      <Container maxWidth="sm">
+        <h1>Peter Wu</h1>
+        <p>
+          Hey there! I&apos;m Peter, and I&apos;m a CS major at Cornell
+          University who likes to make things that work (but this website does not).
+        </p>
+        <IconButton className={styles.portfolioButton} aria-label="delete">
+          <GitHubIcon />
+        </IconButton>
+      </Container>
     </Card>
   </Container>
 );
