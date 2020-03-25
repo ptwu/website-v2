5a4dcfc3a9cce368bf416ba2b67bf0d0b72084df
Peter Wu <peterlingwu@gmail.com>
2020-03-25 18:06:56 -0400

    Updates

---
+++ precache-manifest.d6b9a63742144636ec68eb931099c35e.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1feb2f71fecf1c8b11c3cb678eeeada1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6219b08a159fe22090a4",
+    "url": "/static/css/main.5bdf17b7.chunk.css"
+  },
+  {
+    "revision": "39429fdf9055324be94e",
+    "url": "/static/js/2.9f50ded3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.9f50ded3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6219b08a159fe22090a4",
+    "url": "/static/js/main.f25f791f.chunk.js"
+  },
+  {
+    "revision": "6e7d85ad969107dd34f7",
+    "url": "/static/js/runtime-main.54edee9e.js"
+  },
+  {
+    "revision": "f4bca87fd0d19e61c27dc96299c75f8c",
+    "url": "/static/media/Metropolis-Medium.f4bca87f.otf"
+  },
+  {
+    "revision": "8d49514ad5d989307093ded604a7cac6",
+    "url": "/static/media/Peter_Wu_Resume.8d49514a.pdf"
+  },
+  {
+    "revision": "eddcbbd46528ed2857fcb09a2f2b356c",
+    "url": "/static/media/bg.eddcbbd4.jpg"
+  },
+  {
+    "revision": "1f867066dce63f000e66b45ecb7cd12f",
+    "url": "/static/media/bgvid.1f867066.webm"
+  },
+  {
+    "revision": "d1ea16f7117b4fc67528770554a0f403",
+    "url": "/static/media/classreveal.d1ea16f7.jpg"
+  },
+  {
+    "revision": "46b7a84896c762c4a5792a6121901050",
+    "url": "/static/media/piratemaps.46b7a848.jpg"
+  },
+  {
+    "revision": "4b0c9b16a1bcd7c98d9cf02d802e289b",
+    "url": "/static/media/ptwulogosmall.4b0c9b16.png"
+  },
+  {
+    "revision": "549a2452089bb009e30b02a15aa606c9",
+    "url": "/static/media/samwise.549a2452.jpg"
+  },
+  {
+    "revision": "a09fc7eb8e26e06e453860ffeae8a188",
+    "url": "/static/media/smartsieve.a09fc7eb.jpg"
+  },
+  {
+    "revision": "c5466fd617b8d8cb7a1de778773ca671",
+    "url": "/static/media/trafficsim.c5466fd6.jpg"
+  },
+  {
+    "revision": "fcc6f2b593e1a584134ff67e5931abab",
+    "url": "/static/media/wwpmt.fcc6f2b5.jpg"
+  }
+]);
