3294a40b78b5e4acc2f2cca54b1705ae48dbf71e
Peter Wu <peterlingwu@gmail.com>
2020-03-25 05:07:33 -0400

    Fix grid alignment with Project cards

--- src/components/ProjectsSection/Project.jsx
+++ src/components/ProjectsSection/Project.jsx
@@ -1,45 +1,44 @@
+/* eslint-disable react/forbid-prop-types */
 import React from 'react';
 import {
-  Card, CardActionArea, CardMedia, CardContent, Typography, CardActions, Button,
+  Card, CardMedia, CardContent, Typography, Button,
 } from '@material-ui/core';
 import { PropTypes } from 'prop-types';
+import styles from './Project.module.css';
 
 export default function Project({
-  image, altText, title, desc, linkText,
+  image, altText, title, desc, linkText, url, style,
 }) {
   return (
     <Card>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          alt={altText}
-          height="240"
-          image={image}
-          title={altText}
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            {title}
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p" align="left">
-            {desc}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          {linkText}
-        </Button>
-      </CardActions>
+      <CardMedia
+        component="img"
+        alt={altText}
+        height="240"
+        image={image}
+        title={altText}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="h2">
+          {title}
+        </Typography>
+        <Typography variant="body2" color="textSecondary" component="p" align="left">
+          {desc}
+        </Typography>
+      </CardContent>
+      <Button className={styles.projectButton} style={style} size="large" target="_blank" rel="noopener noreferrer" href={url}>
+        {linkText}
+      </Button>
     </Card>
   );
 }
 
 Project.propTypes = {
-  // eslint-disable-next-line react/forbid-prop-types
   image: PropTypes.any.isRequired,
   altText: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   desc: PropTypes.string.isRequired,
   linkText: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+  style: PropTypes.any.isRequired,
 };
--- src/components/ProjectsSection/ProjectsSection.jsx
+++ src/components/ProjectsSection/ProjectsSection.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid } from '@material-ui/core';
+import { Grid, Container } from '@material-ui/core';
 import styles from './ProjectsSection.module.css';
 import Project from './Project';
 import Samwise from '../../img/samwise.jpg';
@@ -8,38 +8,46 @@ export default () => (
   <div className={styles.sectionContainer}>
     <h6 className={styles.subheading}>cool things I&apos;ve built!</h6>
     <h1 className={styles.heading}>Projects</h1>
-    <Grid container>
-      <Grid item xs={12}>
-        <Grid className={styles.fullwidthGrid} container justify="center" spacing="8">
-          <Grid item>
-            <Project
-              image={Samwise}
-              altText="bruh"
-              title="Samwise"
-              desc="Samwise is a"
-              linkText="Link To Website"
-            />
-          </Grid>
-          <Grid item>
-            <Project
-              image={Samwise}
-              altText="bruh"
-              title="Samwise"
-              desc="Samwise is a"
-              linkText="Link To Website"
-            />
-          </Grid>
-          <Grid item>
-            <Project
-              image={Samwise}
-              altText="bruh"
-              title="Samwise"
-              desc="Samwise is a"
-              linkText="Link To Website"
-            />
+    <Container maxWidth="lg">
+      <Grid container>
+        <Grid item xs={12}>
+          <Grid className={styles.fullwidthGrid} container justify="center" spacing="8">
+            <Grid item xs>
+              <Project
+                image={Samwise}
+                altText="Samwise"
+                title="Samwise"
+                desc="Samwise is a homework + schedule planner for Cornell students, developed by the Cornell Design &amp; Tech Initiative on campus."
+                linkText="Link To Website"
+                url="https://samwise.today/"
+                style={{ background: '#F44336' }}
+              />
+            </Grid>
+            <Grid item xs>
+              <Project
+                image={Samwise}
+                altText="bruh"
+                title="Samwise"
+                desc="Samwise is a"
+                linkText="Link To Website"
+                url="https://samwise.today/"
+                style={{ background: '#F44336' }}
+              />
+            </Grid>
+            <Grid item xs>
+              <Project
+                image={Samwise}
+                altText="bruh"
+                title="Samwise"
+                desc="Samwise is a"
+                linkText="Link To Website"
+                url="https://samwise.today/"
+                style={{ background: '#F44336' }}
+              />
+            </Grid>
           </Grid>
         </Grid>
       </Grid>
-    </Grid>
+    </Container>
   </div>
 );
