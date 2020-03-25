f7a589060f01dfbc36750d8ecee3a47c0b71c411
Peter Wu <peterlingwu@gmail.com>
2020-03-25 15:01:18 -0400

    complete projects styling and first row

--- src/components/ProjectsSection/ProjectsSection.jsx
+++ src/components/ProjectsSection/ProjectsSection.jsx
@@ -3,6 +3,8 @@ import { Grid, Container } from '@material-ui/core';
 import styles from './ProjectsSection.module.css';
 import Project from './Project';
 import Samwise from '../../img/samwise.jpg';
+import SmartSieve from '../../img/smartsieve.jpg';
+import PirateMaps from '../../img/piratemaps.jpg';
 
 export default () => (
   <div className={styles.sectionContainer}>
@@ -25,24 +27,24 @@ export default () => (
             </Grid>
             <Grid item xs>
               <Project
-                image={Samwise}
-                altText="bruh"
-                title="Samwise"
-                desc="Samwise is a"
-                linkText="Link To Website"
-                url="https://samwise.today/"
-                style={{ background: '#F44336' }}
+                image={SmartSieve}
+                altText="SmartSieve"
+                title="SmartSieve"
+                desc="Chrome extension implementing the powerful Clarifai computer vision API to analyze images, flagging them as safe or unsafe for work."
+                linkText="Link"
+                url="https://chrome.google.com/webstore/detail/smart-sieve/gegngkeciggmgpjdahkffpdfgdldeeib?hl=en"
+                style={{ background: '#2693FF' }}
               />
             </Grid>
             <Grid item xs>
               <Project
-                image={Samwise}
-                altText="bruh"
-                title="Samwise"
-                desc="Samwise is a"
-                linkText="Link To Website"
-                url="https://samwise.today/"
-                style={{ background: '#F44336' }}
+                image={PirateMaps}
+                altText="Pirate Maps"
+                title="Pirate Maps"
+                desc="Android mapping app made with Unity3D, made for helping new students navigate through school. Uses Dijkstra's algorithm to find optimal pathways between the &quot;nodes&quot; of school classrooms.               "
+                linkText="Link To Google Play"
+                url="https://play.google.com/store/apps/details?id=com.CSI.HSSPirateMaps"
+                style={{ background: '#388E3C' }}
               />
             </Grid>
           </Grid>
