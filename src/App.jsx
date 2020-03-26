82d62ba025b8dd6754ef652d9ae2aa3b4652e8be
Peter Wu <peterlingwu@gmail.com>
2020-03-26 04:23:42 -0400

    Fix bold styling

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import VideoContainer from './components/VideoContainer/VideoContainer';
 import ProjectsSection from './components/ProjectsSection/ProjectsSection';
 import Footer from './components/Footer/Footer';
 import MetropolisOtf from './font/Metropolis-Medium.otf';
+import MetropolisBoldOtf from './font/Metropolis-Bold.otf';
 import Logo from './img/ptwulogo.png';
 
 const metropolis = {
@@ -26,14 +27,25 @@ const metropolis = {
     'U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF',
 };
 
+const metropolisBold = {
+  fontFamily: 'Metropolis-Bold',
+  fontStyle: 'bold',
+  fontDisplay: 'swap',
+  fontWeight: 700,
+  src: `
+    local('Metropolis-Bold'),
+    url(${MetropolisBoldOtf}) format('opentype')
+  `,
+};
+
 const theme = createMuiTheme({
   typography: {
-    fontFamily: 'Metropolis',
+    fontFamily: ['Metropolis', 'Metropolis-Bold'],
   },
   overrides: {
     MuiCssBaseline: {
       '@global': {
-        '@font-face': [metropolis],
+        '@font-face': [metropolis, metropolisBold],
       },
     },
   },
