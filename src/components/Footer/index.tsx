35d7f06a1b1e74dc35d9759fdce866f6b3c63b37
Peter Wu <peterlingwu@gmail.com>
2020-07-19 03:26:28 -0400

    Update footer for TS

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,7 +3,7 @@ import './index.module.css';
 
 export default (): ReactElement => (
   <footer>
-    Built from scratch with React, Node.js, HTML5, and CSS3.
+    Built from scratch with React, TypeScript, HTML5, and CSS3.
     <br />
     © 2018-2020 Peter Wu. peterlingwu@gmail.com · (609) 508-7717
   </footer>
