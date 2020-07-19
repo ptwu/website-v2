e94e06588d546c8f99c52fac66c0bb8ed47ca4a5
Peter Wu <peterlingwu@gmail.com>
2020-07-19 03:34:13 -0400

    Create prop type for Project and use it

---
+++ src/components/ProjectsSection/Project.tsx
@@ -0,0 +1,43 @@
+/* eslint-disable react/forbid-prop-types */
+import React, { ReactElement } from 'react';
+import {
+  Card, CardMedia, CardContent, Typography, Button,
+} from '@material-ui/core';
+import styles from './Project.module.css';
+
+type Props = {
+  readonly image: string;
+  readonly altText: string;
+  readonly title: string;
+  readonly desc: any;
+  readonly linkText: string;
+  readonly url: string;
+  readonly style: React.CSSProperties;
+};
+
+export default (props: Props): ReactElement => {
+
+  const { image, altText, title, desc, linkText, url, style } = props;
+  return (
+    <Card>
+      <CardMedia
+        component="img"
+        alt={altText}
+        height="240"
+        image={image}
+        title={altText}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="h2">
+          {title}
+        </Typography>
+        <Typography variant="body2" color="textSecondary" component="p" align="left">
+          {desc}
+        </Typography>
+      </CardContent>
+      <Button className={styles.projectButton} style={style} size="large" target="_blank" rel="noopener noreferrer" href={url}>
+        {linkText}
+      </Button>
+    </Card>
+  );
+}
